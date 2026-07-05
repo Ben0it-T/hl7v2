76a16403fe158227af70a50d2798b641073ddc2f
Ben0it-T <37017213+Ben0it-T@users.noreply.github.com>
2026-07-05 08:36:32 +0200

    refactor: extract findProfileSegmentDef to Profile

--- src/Profile/Profile.php
+++ src/Profile/Profile.php
@@ -32,4 +32,40 @@ class Profile
     {
         return $this->definition;
     }
+
+    /**
+     * Find segment definition in profile.
+     *
+     * @param string $segmentName
+     * @return array<mixed,mixed>
+     */
+    public function findSegmentDefinition(string $segmentName): array
+    {
+        return $this->findSegmentDefinitionInGroup($segmentName, $this->definition);
+    }
+
+    /**
+     * Find segment definition in a profile group.
+     *
+     * @param string $segmentName
+     * @param array<mixed,mixed> $segGroup
+     * @return array<mixed,mixed>
+     */
+    private function findSegmentDefinitionInGroup(string $segmentName, array $segGroup): array
+    {
+        foreach ($segGroup as $child) {
+            if ($child['Type'] === 'segment') {
+                if ($segmentName === $child['Name']) {
+                    return $child;
+                }
+            } elseif ($child['Type'] === 'group') {
+                $definition = $this->findSegmentDefinitionInGroup($segmentName, $child['segments']);
+                if ($definition !== []) {
+                    return $definition;
+                }
+            }
+        }
+
+        return [];
+    }
 }
